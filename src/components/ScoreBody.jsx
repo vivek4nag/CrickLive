4c2d922553fda995cf1468c6b91e13fb41bd82fc
viek4nag <vivek4nag@gmail.com>
2024-12-17 16:58:28 +0530

    API called, body & card rendered

--- src/components/ScoreBody.jsx
+++ src/components/ScoreBody.jsx
@@ -1,49 +1,72 @@
 /* eslint-disable no-undef */
 import "react";
-import process from "process";
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
+import ScoreCard from "./ScoreCard";
 
 const ScoreBody = () => {
+  const [scoreData, setScoreData] = useState([]);
+  const APIKEY = import.meta.env.VITE_CRICKET_API_KEY;
 
-    const apiKey = process.env.REACT_APP_CRICKET_API_KEY;
-    console.log(apiKey);
-
-    const getScoreData = async () => {
-        try {
-            const response = await fetch(`https://api.cricapi.com/v1/cricScore?apikey=${apiKey}`)
-            const data = await response.json();
-            console.log(data);
-        } catch (error) {
-            console.log(error);
-        }
-
+  const getScoreData = async () => {
+    try {
+      const response = await fetch(
+        `https://api.cricapi.com/v1/cricScore?apikey=${APIKEY}`
+      );
+      const data = await response.json();
+      console.log(data.data);
+      setScoreData(data.data);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(() => {
+  useEffect(() => {
+    getScoreData();
+  }, []);
 
-        getScoreData()
-    }, [])
+  return (
+    <div className="bg-gradient-to-r from-gray-900 via-black to-gray-900 text-white">
+      {/* upar wala search bar */}
+      <div className="flex justify-center items-center gap-2 pt-10">
+        <input
+          type="text"
+          placeholder="Search for a team, match or series"
+          className="w-3/5 md:w-2/5 p-3 rounded-lg bg-gray-700 text-gray-200 placeholder-gray-400 shadow-md focus:outline-none focus:ring-2 focus:ring-red-500 transition duration-300"
+        />
+        <button className="px-6 py-2 text-xs md:px-7 md:py-3 md:text-lg md:font-semibold bg-gradient-to-r from-red-500 to-pink-600 rounded-lg hover:from-red-600 hover:to-pink-700 transition duration-500">
+          {" "}
+          Search Now
+        </button>
+      </div>
 
+      <div className="flex flex-wrap justify-center p-3">
+        {/* yahn score card aayga */}
 
-
-
-    return (
-        <div className="bg-gradient-to-r from-gray-900 via-black to-gray-900 text-white">
-            <div className="flex justify-center items-center gap-2 pt-10">
-                <input
-                    type="text"
-                    placeholder="Search for a team, match or series"
-                    className="w-3/5 md:w-2/5 p-3 rounded-lg bg-gray-700 text-gray-200 placeholder-gray-400 shadow-md focus:outline-none focus:ring-2 focus:ring-red-500 transition duration-300"
+        {scoreData ? (
+          scoreData.map((item) => {
+            if (item.status !== "Match not started") {
+              return (
+                <ScoreCard
+                  key={item.id}
+                  matchType={item.matchType}
+                  status={item.status}
+                  series={item.series}
+                  t1={item.t1}
+                  t1img={item.t1img}
+                  t2={item.t2}
+                  t2img={item.t2img}
+                  t1s={item.t1s}
+                  t2s={item.t2s}
                 />
-                <button className="px-6 py-2 text-xs md:px-7 md:py-3 md:text-lg md:font-semibold bg-gradient-to-r from-red-500 to-pink-600 rounded-lg hover:from-red-600 hover:to-pink-700 transition duration-500">
-                    {" "}
-                    Search Now
-                </button>
-            </div>
-            <div></div>
-        </div>
-    );
+              );
+            }
+          })
+        ) : (
+          <p>Data not found</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ScoreBody;
--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -1,8 +1,51 @@
+/* eslint-disable react/prop-types */
+import teamLogo from "../assets/image.png";
 
-const ScoreCard = () => {
+const ScoreCard = ({
+  matchType,
+  key,
+  status,
+  series,
+  t1,
+  t2,
+  t1img,
+  t2img,
+  t1s,
+  t2s,
+}) => {
   return (
-    <div>ScoreCard</div>
-  )
-}
+    <div
+      id={key}
+      className="w-80 h-auto bg-gray-400 rounded-lg p-4 shadow-lg m-2"
+    >
+      <h2 className="text-lg font-bold pb-2 text-center text-black">
+        {series}
+      </h2>
+      <p className="text-gray-900 text-center font-semibold pb-2">
+        match type: {matchType}
+      </p>
+      <div className="flex justify-between items-center">
+        <div className="flex flex-col items-center">
+          <img src={t1img || teamLogo} className="h-12 w-12 rounded-full" />
+          <p className="text-center font-medium text-blue-800">{t1}</p>
+          <p className="text-xl text-green-900 pt-2 font-bold">
+            {t1s ? t1s : "-"}
+          </p>
+        </div>
+        <span className="text-black">VS</span>
+        <div className="flex flex-col items-center">
+          <img src={t2img || teamLogo} className="h-12 w-12 rounded-full" />
+          <p className="text-center font-medium text-blue-800">{t2}</p>
+          <p className="text-xl text-green-900 pt-2 font-bold">
+            {t2s ? t2s : "-"}
+          </p>
+        </div>
+      </div>
+      <p className="text-lg font-semibold text-blue-950 text-center pt-4">
+        {status}
+      </p>
+    </div>
+  );
+};
 
-export default ScoreCard
+export default ScoreCard;
